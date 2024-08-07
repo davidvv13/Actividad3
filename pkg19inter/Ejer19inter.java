147db360742e26f6c587cab564f93fa35129d143
davidvv13 <161548966+davidvv13@users.noreply.github.com>
2024-08-06 22:25:00 -0500

    Add files via upload

---
+++ pkg19inter/Ejer19inter.java
@@ -0,0 +1,9 @@
+package ejer.pkg19inter;
+
+public class Ejer19inter {
+
+    public static void main(String[] args) {
+        Ventana ventana= new Ventana();
+        ventana.show(true);
+    }   
+    }
